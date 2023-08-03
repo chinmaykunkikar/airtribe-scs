910b601d2757f315141d20956d51cbacf5fbb7c1
Chinmay Kunkikar <chinmay.kunkikar@gmail.com>
2023-08-04 02:51:17 +0530

    middleware: Use JWT_SECRET env variable

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config/env.config");
 
 const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization");
@@ -6,7 +7,7 @@ const authMiddleware = (req, res, next) => {
     return res.status(401).json({ error: "No token, authorization denied" });
   }
   try {
-    const decoded = jwt.verify(token, "your_secret_key");
+    const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
